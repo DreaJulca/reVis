1007a259e56c3e5f375eb1617ac7d43ff296afb1
Andrea Julca <andrea.julca@gmail.com>
2016-07-26 10:37:04 -0400

    Update Parse_BEAhist.r

--- R/Parse_BEAhist.r
+++ R/Parse_BEAhist.r
@@ -5,10 +5,15 @@ library(data.table)
 	
 	allFolds <- list.dirs(path = myPath)
 	
-	#allFiles <- 
-
-
+	allFiles <- rbindlist(lapply(allFolds, function(thisPath){
+		theseFiles <- data.table(list.files(path = thisPath, full.names = TRUE));
+		return(theseFiles);
+	}));
+	
+	allCSVs <- allFiles[tolower(substr(V1, nchar(V1)-2, nchar(V1))) == 'csv']
+	
  #In this case, lines 1-6 are meta; lines 
- readDT <- fread('C:/users/niacj1/documents/reVis/xls/NIPA/2016/q1/advance_april-28-2016/section7all_xlssht39.csv')
+ readDT <- fread(paste0('C:/users/',userID, '/documents/reVis/xls/NIPA/2016/q1/advance_april-28-2016/section7all_xlssht39.csv'))
  cleanDT <- readDT[!is.na(`NA..3`)]
  nameDT <- names(readDT)[2]
+ 
